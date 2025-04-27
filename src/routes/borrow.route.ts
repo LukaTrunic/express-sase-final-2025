04b63ea937e2905f54d0362183a42b49fd6dfc6b
LukaTrunic <lukatrunic@gmail.com>
2025-04-28 00:26:15 +0200

    CRUD

--- src/routes/borrow.route.ts
+++ src/routes/borrow.route.ts
@@ -6,10 +6,56 @@ export const BorrowRoute = Router()
 
 BorrowRoute.get('/', async (req: any, res) => {
     try {
-        console.log(req.user)
         res.json(await BorrowService.getBorrows(req.user.id))
     } catch (e) {
-        console.log(e)
+        sendError(res, e)
+    }
+})
+
+BorrowRoute.get('/:id', async (req: any, res) => {
+    try {
+        const id = Number(req.params.id)
+        res.json(await BorrowService.getBorrowById(req.user.id, id))
+    } catch (e) {
+        sendError(res, e)
+    }
+})
+
+BorrowRoute.post('/', async (req: any, res) => {
+    try {
+        await BorrowService.createBorrow(req.user.id, req.body)
+        res.status(204).send()
+    } catch (e) {
+        sendError(res, e)
+    }
+})
+
+BorrowRoute.put('/:id/return', async (req: any, res) => {
+    try {
+        const id = Number(req.params.id)
+        await BorrowService.makeBorrowReturned(req.user.id, id)
+        res.status(204).send()
+    } catch (e) {
+        sendError(res, e)
+    }
+})
+
+BorrowRoute.put('/:id', async (req: any, res) => {
+    try {
+        const id = Number(req.params.id)
+        await BorrowService.updateBorrow(req.user.id, id, req.body)
+        res.status(204).send()
+    } catch (e) {
+        sendError(res, e)
+    }
+})
+
+BorrowRoute.delete('/:id', async (req: any, res) => {
+    try {
+        const id = Number(req.params.id)
+        await BorrowService.deleteBorrow(req.user.id, id)
+        res.status(204).send()
+    } catch (e) {
         sendError(res, e)
     }
 })
