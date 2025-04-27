dc295fd03d6d9dda92ccdd0b3018684310ec44e1
LukaTrunic <lukatrunic@gmail.com>
2025-04-27 13:15:08 +0200

    Renamed Book Route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import { BookRoute } from './flight.route'
+import { BookRoute } from './book.route'
 
 const app = express()
 app.use(cors())
