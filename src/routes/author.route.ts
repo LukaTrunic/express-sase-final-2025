ac17c48aa9acf6a1db2fdb47de520b3bb7465fd1
LukaTrunic <lukatrunic@gmail.com>
2025-04-27 20:48:39 +0200

    Added get author by id

--- src/routes/author.route.ts
+++ src/routes/author.route.ts
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { AuthorService } from "../services/author.service";
+import { sendError } from "../utils";
 
 export const AuthorRoute = Router()
 
 AuthorRoute.get('/', async (req, res) => {
-    res.json(await AuthorService.getAuthor())
+    try {
+        res.json(await AuthorService.getAuthors())
+    } catch(e) {
+        sendError(res, e)
+    }
+})
+
+AuthorRoute.get('/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id)
+        res.json(await AuthorService.getAuthorById(id))
+    } catch(e) {
+        sendError(res, e)
+    }
 })
--- src/services/author.service.ts
+++ src/services/author.service.ts
@@ -1,19 +1,36 @@
+import { IsNull } from "typeorm";
 import { AppDataSource } from "../db";
 import { Author } from "../entities/Author";
 
 const repo = AppDataSource.getRepository(Author)
 
 export class AuthorService {
-    static async getAuthor() {
+    static async getAuthors() {
         return await repo.find({
             select: {
                 authorId: true,
                 name: true,
-                website: true
+                website: true,
+                createdAt: true,
+                updatedAt: true
             },
             where: {
-                active:true
+                deletedAt: IsNull()
             }
         })
+    }
+
+    static async getAuthorById(id: number) {
+        const data = await repo.findOne({
+            where: {
+                authorId: id,
+                deletedAt: IsNull()
+            }
+        })
+
+        if (data == undefined)
+            throw new Error('NOT_FOUND')
+
+        return data
     }
 }
